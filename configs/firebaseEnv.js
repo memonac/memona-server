5d6912cb35267311efeb426035f1e6ca9343df61
woojeong kong <coolstevevc@gmail.com>
2022-02-04 16:38:46 +0900

    chore: firebase constant handling

---
+++ configs/firebaseEnv.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG_INFO = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.IREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+module.exports = FIREBASE_CONFIG_INFO;
---
+++ routes/middlewares/verifyToken.js
@@ -0,0 +1,6 @@
+const { authenication } = require("../../configs/firebase");
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (req, res, next) => {};
+
+module.exports = verifyToken;

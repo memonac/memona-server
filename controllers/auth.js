ed402a26f20ae68539e6368ec4d8093c2a191972
woojeong kong <coolstevevc@gmail.com>
2022-02-05 17:29:04 +0900

    fix: add signupController

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ exports.getLogin = async (req, res, next) => {
   const { userInfo, accessToken, refreshToken } = res.locals;
 
   const cookieOptions = {
-    path: "/",
     httpOnly: true,
   };
 
@@ -17,6 +16,49 @@ exports.getLogin = async (req, res, next) => {
 
   try {
     await userService.createUser(userInfo);
+
+    res.json({
+      result: "success",
+    });
+  } catch (err) {
+    if (err.name === "MongoServerError" || err.name === "ValidationError") {
+      res.status(400).json({
+        result: "fail",
+        error: {
+          message: "DataBase Error",
+        },
+      });
+
+      return;
+    }
+
+    next(createError(500, "Invalid Server Error"));
+  }
+};
+
+exports.postSignup = async (req, res, next) => {
+  const { data } = req.body;
+
+  const { userInfo, accessToken, refreshToken } = res.locals;
+
+  const cookieOptions = {
+    httpOnly: true,
+  };
+
+  res.cookie("token", accessToken, cookieOptions);
+
+  if (userInfo) {
+    userInfo.refreshToken = refreshToken;
+  }
+
+  userInfo.name = data.name;
+
+  try {
+    await userService.createUser(userInfo);
+
+    res.json({
+      result: "success",
+    });
   } catch (err) {
     if (err.name === "MongoServerError" || err.name === "ValidationError") {
       res.status(400).json({
